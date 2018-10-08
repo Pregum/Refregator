cc1dbbfabbf03c1ec320cff153d063970fbda558
Pregum <pregum.y@gmail.com>
2018-10-09 01:44:42 +0900

    [add] 編集用のPageを追加
    ダッシュボードからカレンダーや編集画面に遷移するためのイベントを実装した。

---
+++ Common/RadioButtonUriConverter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+using MVVM_Refregator.Model;
+
+namespace MVVM_Refregator.Common
+{
+    public class RadioButtonUriConverter : IValueConverter
+    {
+        /// <summary>
+        /// View -> ViewModelへの変換
+        /// </summary>
+        /// <param name="value">ソースの値</param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            var parameterString = parameter as string;
+            if (parameterString == null)
+            {
+                return System.Windows.DependencyProperty.UnsetValue;
+            }
+
+            if (Enum.IsDefined(value.GetType(), value) == false)
+            {
+                return System.Windows.DependencyProperty.UnsetValue;
+            }
+
+            object parameterValue = Enum.Parse(value.GetType(), parameterString);
+
+            return (int)parameterValue == (int)value;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var parameterString = parameter as string;
+            return parameterString == null ? System.Windows.DependencyProperty.UnsetValue : Enum.Parse(targetType, parameterString);
+        }
+    }
+}
---
+++ Model/DisplayPageStatus.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_Refregator.Model
+{
+    /// <summary>
+    /// 表示されているページを表すEnum
+    /// </summary>
+    public enum DisplayPageStatus
+    {
+        /// <summary>
+        /// ダッシュボード(最初のページ)
+        /// </summary>
+        DashBoard = 1,
+        /// <summary>
+        /// カレンダーページ
+        /// </summary>
+        Calendar = 2,
+        /// <summary>
+        /// 編集ページ
+        /// </summary>
+        Editor = 3
+    }
+}
