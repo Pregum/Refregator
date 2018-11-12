fd09b94f088134aadd3e5ff4096745d541728b54
Pregum <pregum.y@gmail.com>
2018-11-13 03:50:00 +0900

    [update] コメントの削除等のコードの整理

--- ViewModel/StepOfFoodLimitDateViewModel.cs
+++ ViewModel/StepOfFoodLimitDateViewModel.cs
@@ -14,24 +14,16 @@ namespace MVVM_Refregator.ViewModel
 {
     public class StepOfFoodLimitDateViewModel : BindableBase
     {
-        //private FoodLimitDateEditStep _foodLimitDateEditStep;
-
         private WorkStepModel _workStepModel;
 
-        //public ReactiveProperty<DateTime> LimitDate { get; }
         public ReactiveProperty<FoodModel> Food { get; }
 
+        /// <summary>
+        /// ctor
+        /// </summary>
         public StepOfFoodLimitDateViewModel()
         {
-            //this._foodLimitDateEditStep = FoodLimitDateEditStep.GetInstance();
             this._workStepModel = WorkStepModel.GetInstance();
-
-            //this.LimitDate = this._foodLimitDateEditStep.ToReactivePropertyAsSynchronized(
-            //    m => m.LimitDate,
-            //    convert => convert,
-            //    convertBack => convertBack,
-            //    ReactivePropertyMode.DistinctUntilChanged | ReactivePropertyMode.RaiseLatestValueOnSubscribe,
-            //    false);
 
             this.Food = this._workStepModel.ToReactivePropertyAsSynchronized(
                 m => m.ManipulateFood,
